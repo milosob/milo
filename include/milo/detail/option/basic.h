bcb2c3831d37d99739aa0a8720d8a55d14ce32cd
milosob <git@milosob.com>
2022-11-20 11:55:58 +0100

    new option backend

--- include/milo/detail/option/basic.h
+++ include/milo/detail/option/basic.h
@@ -3,17 +3,15 @@
 #pragma once
 
 
-#include <milo/common.h>
 #include <milo/traits.h>
 
 
 namespace milo::detail
 {
     template<
-        typename t_option
+        typename t_type
     >
     struct option_oftype
-        : private common::disabled
     {
         struct properties
         {
@@ -22,33 +20,14 @@ namespace milo::detail
             using option_oftype [[maybe_unused]] = int;
         };
         
-        using type [[maybe_unused]] = t_option;
+        using type [[maybe_unused]] = t_type;
     };
     
     template<
-        typename t_0
-    >
-    struct option_oftype_is
-        : traits::boolean<
-            t_0,
-            (
-                requires
-                {
-                    typename t_0::properties::option;
-                    typename t_0::properties::option_oftype;
-                    typename t_0::type;
-                }
-            )
-        >
-    {
-    };
-    
-    template<
-        typename t_option,
-        t_option t_value
+        typename t_type,
+        t_type t_value
     >
     struct option_ofvalue
-        : private common::disabled
     {
         struct properties
         {
@@ -58,24 +37,203 @@ namespace milo::detail
         };
         
         static
-        constexpr t_option value = t_value;
+        constexpr t_type value = t_value;
     };
     
     template<
-        typename t_0
+        typename t_option
     >
-    struct option_ofvalue_is
+    struct option_oftype_is
         : traits::boolean<
-            t_0,
+            t_option,
             (
                 requires
                 {
-                    typename t_0::properties::option;
-                    typename t_0::properties::option_ofvalue;
-                    t_0::value;
+                    typename t_option::properties::option;
+                    typename t_option::properties::option_oftype;
+                    typename t_option::type;
                 }
             )
         >
     {
+    };
+    
+    template<
+        typename t_option
+    >
+    constexpr auto option_oftype_is_v = option_oftype_is<t_option>::value;
+    
+    template<
+        typename t_option
+    >
+    struct option_ofvalue_is
+        : traits::boolean<
+            t_option,
+            (
+                requires
+                {
+                    typename t_option::properties::option;
+                    typename t_option::properties::option_ofvalue;
+                    t_option::value;
+                }
+            )
+        >
+    {
+    };
+    
+    template<
+        typename t_option
+    >
+    constexpr auto option_ofvalue_is_v = option_ofvalue_is<t_option>::value;
+    
+    template<
+        template<typename> typename t_solver,
+        typename t_default,
+        typename... t_options
+    >
+    struct option_query
+    {
+    private:
+        
+        using expression_type = traits::disjunction<
+            t_solver<t_options>...,
+            t_solver<t_default>
+        >;
+        
+        static_assert(expression_type::value);
+    
+    public:
+        
+        using type = typename expression_type::type;
+    };
+    
+    template<
+        typename t_properties,
+        typename t_type
+    >
+    struct option_ofvalue_suite
+    {
+    private:
+        
+        template<
+            t_type t_value
+        >
+        struct option_impl
+            : option_ofvalue<t_type, t_value>
+        {
+            /*            struct properties
+                            : t_properties,
+                              option_ofvalue<t_type, t_value>::properties
+                        {
+                        };*/
+        };
+    
+    public:
+        
+        template<
+            t_type t_value
+        >
+        using option = option_impl<t_value>;
+        
+        template<
+            typename t_option
+        >
+        using solver = traits::boolean<
+            t_option,
+            (
+                requires
+                {
+                    requires option_ofvalue_is_v<t_option>;
+                    requires traits::is_same_v<
+                        traits::del_constant_t<
+                            decltype(t_option::value)
+                        >,
+                        t_type
+                    >;
+                    requires traits::is_same_v<
+                        t_option,
+                        option<t_option::value>
+                    >;
+                }
+            )
+        >;
+        
+        template<
+            typename t_default,
+            typename... t_options
+        >
+        using query = typename option_query<
+            solver,
+            t_default,
+            t_options...
+        >::type;
+        
+        template<
+            typename t_default,
+            typename... t_options
+        >
+        static
+        constexpr auto query_v = query<t_default, t_options...>::value;
+    };
+    
+    template<
+        typename t_properties
+    >
+    struct option_oftype_suite
+    {
+    private:
+        
+        template<
+            typename t_type
+        >
+        struct option_impl
+            : option_oftype<t_type>
+        {
+            /*            struct properties
+                            : t_properties,
+                              option_oftype<t_type>::properties
+                        {
+                        };*/
+        };
+    
+    public:
+        
+        template<
+            typename t_type
+        >
+        using option = option_impl<t_type>;
+        
+        template<
+            typename t_option
+        >
+        using solver = traits::boolean<
+            t_option,
+            (
+                requires
+                {
+                    requires option_oftype_is_v<t_option>;
+                    requires traits::is_same_v<
+                        t_option,
+                        option<typename t_option::type>
+                    >;
+                }
+            )
+        >;
+        
+        template<
+            typename t_default,
+            typename... t_options
+        >
+        using query = typename option_query<
+            solver,
+            t_default,
+            t_options...
+        >::type;
+        
+        template<
+            typename t_default,
+            typename... t_options
+        >
+        using query_t = typename query<t_default, t_options...>::type;
     };
 }
