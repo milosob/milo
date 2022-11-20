65a071c16b3c0a0c61f358ff3fd4e2f83c7dd431
milosob <git@milosob.com>
2022-11-20 15:04:02 +0100

    add impl options

--- include/milo/option.h
+++ include/milo/option.h
@@ -9,5 +9,14 @@
 namespace milo::option
 {
     template<size_t t_value>
-    using digest_size = milo::detail::option_digest_size<t_value>;
+    using digest_size = milo::detail::option_digest_size_suite::option<t_value>;
+    
+    template<typename t_type>
+    using impl_scope = milo::detail::option_impl_scope_suite::option<t_type>;
+    
+    template<typename t_type>
+    using impl_constexpr = milo::detail::option_impl_constexpr_suite::option<t_type>;
+    
+    template<typename t_type>
+    using impl_runtime = milo::detail::option_impl_runtime_suite::option<t_type>;
 }
