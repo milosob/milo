f2b575f522e92f876e87b73c69b4470ce4253768
milosob <git@milosob.com>
2022-11-24 23:34:38 +0100

    remove namespace prefix

--- include/milo/option.h
+++ include/milo/option.h
@@ -9,7 +9,10 @@
 namespace milo::option
 {
     template<size_t t_value>
-    using digest_size = milo::detail::option_digest_size_suite::option<t_value>;
+    using digest_size = detail::option_digest_size_suite::option<t_value>;
+    
+    template<typename t_type>
+    using impl_domain = detail::option_impl_domain_suite::option<t_type>;
     
     template<typename t_type>
     using impl_chooser = detail::option_impl_chooser_suite::option<t_type>;
@@ -18,11 +21,8 @@ namespace milo::option
     using impl_invoker = detail::option_impl_invoker_suite::option<t_type>;
     
     template<typename t_type>
-    using impl_domain = milo::detail::option_impl_domain_suite::option<t_type>;
+    using impl_cpltime = detail::option_impl_cpltime_suite::option<t_type>;
     
     template<typename t_type>
-    using impl_cpltime = milo::detail::option_impl_cpltime_suite::option<t_type>;
-    
-    template<typename t_type>
-    using impl_runtime = milo::detail::option_impl_runtime_suite::option<t_type>;
+    using impl_runtime = detail::option_impl_runtime_suite::option<t_type>;
 }
