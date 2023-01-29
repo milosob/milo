fd2f444605b66bee68968456c734c4c3d9579110
milosob <git@milosob.com>
2023-01-29 20:56:49 +0100

    local start label

--- include/milo/primitive/detail/cipher/chacha/chacha_impl_hw_x86_64.asm
+++ include/milo/primitive/detail/cipher/chacha/chacha_impl_hw_x86_64.asm
@@ -35,7 +35,6 @@ global milo_primitive_detail_cipher_chacha_impl_hw_x86_64_ssse_3_ietf_generate
 %xdefine    stack_size              (stack_size + 16 * 16)
 
 ; Variables.
-%define     x_state_eval(i)         xmm %+ i
 %define     g_rounds                abi_arg_0
 %define     g_rounds_cnt            rbx
 %define     g_blocks                abi_arg_1
@@ -46,6 +45,7 @@ global milo_primitive_detail_cipher_chacha_impl_hw_x86_64_ssse_3_ietf_generate
 %define     m_dst(i)                [g_dst + (i)]
 %define     g_src                   abi_arg_4
 %define     m_src(i)                [g_src + (i)]
+%define     x_state_eval(i)         xmm %+ i
 
 %macro  chacha_ssse_3_rounds 18
 ; Args:
@@ -268,6 +268,7 @@ section .rodata
     db      0x0f, 0x0c, 0x0d, 0x0e
 section .text
 
+.start:
     push    rbp
     push    rbx
     mov     rbp,    rsp
